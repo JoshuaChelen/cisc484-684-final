753295c2d2f8c57a43b99f616eeb064574200101
Grace <grace.c.gulick@gmail.com>
2026-04-26 11:44:06 -0400

    built loaders and visalization

--- src/01_data_setup/07_hair_remover_filter.py
+++ src/01_data_setup/07_hair_remover_filter.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 #paths
 INPUT_DIR  = os.path.join("..", "..", "data", "HAM10000_images_color_normalized")
 #OUTPUT_DIR = os.path.join("..", "..", "data", "HAM10000_images_hair_removed")
-OUTPUT_DIR=os.path.join("..", "..", "data","test_20_hair_removed" )
+OUTPUT_DIR=os.path.join("..", "..", "data","HAM10000_images_hair_removed" )
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
@@ -20,16 +20,16 @@ def remove_hair(image):
     gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     #using blackhat to highlight thin lines
-    kernel=cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    kernel=cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     blackhat= cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, kernel)
     #thresholding to create hair mask
-    _, hair_mask=cv2.threshold(blackhat, 40, 255, cv2.THRESH_BINARY)
+    _, hair_mask=cv2.threshold(blackhat, 30, 255, cv2.THRESH_BINARY)
     #thickening mask
     #hair_mask=cv2.dilate(hair_mask, np.ones((3,3), np.uint8), iterations=1)
     #fill in mask 
     result=cv2.inpaint(image, hair_mask, 1, cv2.INPAINT_TELEA)
 
-    return result, hair_mask
+    return result
 
   
 
@@ -40,42 +40,43 @@ skipped=0
 
 #files = [f for f in os.listdir(INPUT_DIR) if f.endswith(".jpg")]
 files = [f for f in os.listdir(INPUT_DIR) if f.endswith(".jpg")]
-random.seed(42)
-files = random.sample(files, min(20, len(files)))
+#random.seed(42)
+#files = random.sample(files, min(20, len(files)))
 #displaying testing samples
-for fname in files:
-    before_path = os.path.join(INPUT_DIR, fname)
-    after_path  = os.path.join(OUTPUT_DIR, fname)
 
-    before = cv2.imread(before_path)
-    after  = cv2.imread(after_path)
+# for fname in files:
+#     before_path = os.path.join(INPUT_DIR, fname)
+#     after_path  = os.path.join(OUTPUT_DIR, fname)
 
-    if before is None or after is None:
-        print(f"Skipping {fname}")
-        continue
+#     before = cv2.imread(before_path)
+#     after  = cv2.imread(after_path)
 
-    # Convert BGR → RGB for correct display
-    before = cv2.cvtColor(before, cv2.COLOR_BGR2RGB)
-    after  = cv2.cvtColor(after, cv2.COLOR_BGR2RGB)
+#     if before is None or after is None:
+#         print(f"Skipping {fname}")
+#         continue
 
-    plt.figure(figsize=(8,4))
+#     # Convert BGR → RGB for correct display
+#     before = cv2.cvtColor(before, cv2.COLOR_BGR2RGB)
+#     after  = cv2.cvtColor(after, cv2.COLOR_BGR2RGB)
 
-    plt.subplot(1,2,1)
-    plt.imshow(before)
-    plt.title("Before")
-    plt.axis("off")
+#     plt.figure(figsize=(8,4))
 
-    plt.subplot(1,2,2)
-    plt.imshow(after)
-    plt.title("After")
-    plt.axis("off")
+#     plt.subplot(1,2,1)
+#     plt.imshow(before)
+#     plt.title("Before")
+#     plt.axis("off")
 
-    plt.suptitle(fname)
-    plt.show()
+#     plt.subplot(1,2,2)
+#     plt.imshow(after)
+#     plt.title("After")
+#     plt.axis("off")
 
-print("Testing these files:")
-for f in files:
-    print(f)
+#     plt.suptitle(fname)
+#     plt.show()
+
+# print("Testing these files:")
+# for f in files:
+#     print(f)
 
 for fname in files:
     src_path = os.path.join(INPUT_DIR, fname)
